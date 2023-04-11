493e5e5b8c19d41bce5ab8c369ddadc6033372f7
Anna <basbassa@gmail.com>
2023-04-11 20:49:08 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    const currentTime = Math.round(player.currentTime());
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
